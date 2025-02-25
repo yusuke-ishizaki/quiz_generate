e82a99200b2eb91485ba9b3584ea35cac0e8c24c
yusuke-ishizaki <isizaki1114@gmail.com>
2025-02-25 22:20:25 +0900

    Change auth

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# 環境変数からユーザー名とパスワードを取得
+admin_user = os.getenv("ADMIN")
+admin_password = os.getenv("PASSWORD")
 
 def main():
     st.title("ログイン画面")
@@ -7,10 +14,10 @@ def main():
     username = st.text_input("ユーザー名")
     password = st.text_input("パスワード", type="password")
     
-    # シンプルな認証（本番環境では安全な認証方法を使用）
+    # 環境変数を使用した認証
     if st.button("ログイン"):
-        if username == "admin" and password == "password":
-            st.success("Hello!")
+        if username == admin_user and password == admin_password:
+            st.success("ログイン成功！")
         else:
             st.error("ユーザー名またはパスワードが間違っています。")
 
